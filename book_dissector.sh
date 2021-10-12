074fba60f0ced19c6edb576dbdb0b5d6a83755ec
Liam <liam.brockley@gmail.com>
2021-10-11 21:31:19 -0600

    Update book_dissector.sh

--- book_dissector.sh
+++ book_dissector.sh
@@ -2,23 +2,22 @@
 ##Before running, Set the book's name, e.g.
 ##BOOK=[book_name] (without the .txt extension)
 #
-#
-##Get rid of most punctuation and paragraphs and split the book into single words on each line. Output this to [book_name]_words.txt
+#Get rid of most punctuation and paragraphs and split the book into single words on each line. Output this to [book_name]_words.txt
 #
 cat $BOOK".txt" | tr -d '[:punct:]'| sed -e 's/“//' -e 's/”//' -e 's/—//' -e '/^[[:space:]]*$/d' -e 's/\s/\n/g' | sed  '/^$/d' > $BOOK"_words.txt"
 #
-##Make a file where the words are sorted alphabetically
+#Make a file where the words are sorted alphabetically
 #
 sort $BOOK"_words.txt" > $BOOK"_words_alpha.txt"
 #
-##Make a file where unique words are sorted by frequency and have number of occurrences listed beside them
+#Make a file where unique words are sorted by frequency and have number of occurrences listed beside them
 #
 uniq -c $BOOK"_words_alpha.txt" | sort -n > $BOOK"_words_freq.txt"
 #
-##Make a file where unique words are sorted by length
+#Make a file where unique words are sorted by length
 #
 uniq $BOOK"_words_alpha.txt" | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- > $BOOK"_words_length.txt"
 #
-##Make a file with the 200 most frequent words with length of at least 6 (adjust numbers as needed)
+#Make a file with the 200 most frequent words with length of at least 6 (adjust numbers as needed)
 #
 grep -w '\w\{6,\}' $BOOK"_words_freq.txt" | tail -n 200 | tac > $BOOK"_words_top200_over6.txt"
